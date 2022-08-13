918ba7fc267b6b88a48db2f79958ee4a9c614534
Kuba <ttroczynski5@gmail.com>
2022-08-13 13:24:44 +0200

    pierwsz

--- pull.php
+++ pull.php
@@ -1,4 +1,10 @@
-f ( $_POST['payload'] ) {
+<?php
+
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+//hello
+
+
+if ( $_POST['payload'] ) {
 putenv('PATH=/usr/local/bin');
 echo shell_exec('cd /home4/smakolyk/public_html/test_dwa && /usr/bin/git pull origin main 2>&1');
 echo shell_exec('/usr/bin/whoami 2>&1');
@@ -6,5 +12,3 @@ echo 'mateusz2';
 }
 //kubaa
 ?>
-
-
