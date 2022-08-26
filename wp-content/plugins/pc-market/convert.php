d93a0121931a2aef776b7657fc7c3140fd74d53c
kuba <info@weblead.pl>
2022-08-26 18:13:39 +0200

    pierwszy

--- wp-content/plugins/pc-market/convert.php
+++ wp-content/plugins/pc-market/convert.php
@@ -3,11 +3,7 @@
 $fields = array("towar_id","kod","cku","do_usuniecia","czy_opakowanie","nazwa","skrot","typ_towaru","vat_id","pkwiu","sww_indeks","asortyment_id","jm_id","waga","wysokosc","szerokosc","glebokosc","il_w_zgrzewce","il_paleta","il_warstwa","il_kg_litrow","kod_CN","podlega_OO","podlega_MPP","status_do_zamowien","nowosc_od","nowosc_przez","koszty_transportu","koszty_przechowywania","koszty_inne","min_cena_sprzedazy_PCPOS","aktywny_w_SI","nazwa_w_SI","cena_zakupu","cena_detal","cena_hurtowa","cena_nocna","cena_dodatkowa","cena_detal_przed_prom","cena_hurtowa_przed_prom","cena_nocna_przed_prom","cena_dodatkowa_przed_prom","marza_suger","narzut_nocny","rabat_hurtowy","rabat_dodatkowy","status_ceny","opakowanie_id","ilosc_w_opakowaniu","czy_tandem","czy_karton","czy_artykul","artykul_id","kategoria_id","producent_id","dostawca_id","dost_id","opis1","opis2","opis3","opis4","notatki","uwagi_do_dostaw","nr_drukarki","folder_zdjec","plik_zdjecia","magazyn_id","stan_magazynu","stan_magazynu_min","stan_magazynu_max","blokada_stanu","rezerwacja_ilosci","magazyn_id","stan_magazynu","stan_magazynu_min","stan_magazynu_max","blokada_stanu","rezerwacja_ilosci","magazyn_id","stan_magazynu","stan_magazynu_min","stan_magazynu_max","blokada_stanu","rezerwacja_ilosci","kody_dodatkowe","parametry","data_aktualizacji");
 
 
-$list=array();
-$kolumny=array();
-$delimiter = ',';
-$enclosure = '"';
-$stan=array();
+
 //rffg
 
 function createCsv($xml, $f, $fields)
@@ -25,6 +21,11 @@ function createCsv($xml, $f, $fields)
     global $stan;
     $rows = array();
     $data = array();
+    $list=array();
+    $kolumny=array();
+    $delimiter = ',';
+    $enclosure = '"';
+    $stan=array();
 
     foreach ($xml->children() as $item) {
         
